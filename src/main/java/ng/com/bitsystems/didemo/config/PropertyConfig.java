bb0ad0c518b54667a27ed1bc75c541b6e542f1b9
Ezenna Kelechi <charlesezenna@gmail.com>
2020-07-17 15:54:41 +0100

    defined enviroment variable within intelliJ to override properties in datasource.properties file

--- src/main/java/ng/com/bitsystems/didemo/config/PropertyConfig.java
+++ src/main/java/ng/com/bitsystems/didemo/config/PropertyConfig.java
@@ -1,15 +1,21 @@
 package ng.com.bitsystems.didemo.config;
 
 import ng.com.bitsystems.didemo.examplebean.FakeDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
+
+    @Autowired
+    Environment environment;
+
     @Value("${bitsystems.user}")
     String user;
 
@@ -24,7 +30,7 @@ public class PropertyConfig {
         FakeDataSource fakeDataSource = new FakeDataSource();
         fakeDataSource.setDburl(dburl);
         fakeDataSource.setPassword(password);
-        fakeDataSource.setUser(user);
+        fakeDataSource.setUser(environment.getProperty("USERNAME"));
         return fakeDataSource;
     }
 
